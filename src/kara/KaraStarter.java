c25cb4e6a812957e2975a706bc40bb0db00892c3
SpaxR <55739719+SpaxR@users.noreply.github.com>
2020-09-18 12:01:43 +0200

    Implemented abstract Starter class

---
+++ src/kara/KaraStarter.java
@@ -0,0 +1,30 @@
+package kara;
+
+import ch.karatojava.kapps.Application;
+import ch.karatojava.kapps.world.World;
+import javakara.JavaKaraProgram;
+
+import java.io.File;
+
+
+public abstract class KaraStarter extends JavaKaraProgram {
+
+
+    private final Application _app;
+
+    public KaraStarter() {
+        this._app = this.createApplication();
+        this._app.startApplication(new String[0]);
+//        this.init();
+    }
+
+    public void loadWorld(String file) throws Exception {
+        // Lade Datei
+        this._app.getWorldEditorFacade().getWorldEditor().load(new File(file));
+        // Lade Welt
+        this.setWorld((World) this._app.getWorldEditorFacade().getWorldEditor().getContent());
+    }
+
+    public abstract void runKara();
+
+}
