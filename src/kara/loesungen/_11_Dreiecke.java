1a6978847c713dcb104ba40267b635c791a6361e
SpaxR <55739719+SpaxR@users.noreply.github.com>
2020-09-18 17:07:09 +0200

    Improved Task 11 with dynamic height and position of kara

--- src/kara/loesungen/_11_Dreiecke.java
+++ src/kara/loesungen/_11_Dreiecke.java
@@ -9,15 +9,27 @@ public class _11_Dreiecke extends KaraStarter {
         starter.runKara("aufgaben/_11_dreiecke/world1.world");
     }
 
+    private int calculatePossibleLines() {
+        // Maximal width/2, or height if world is smaller
+        return Math.min((this.world.getSizeX()) / 2, this.world.getSizeY());
+    }
+
+    private void setKaraToPosition() {
+        kara.setPosition(this.world.getSizeX() / 2, 0);
+    }
+
     @Override
     public void runKara() {
         boolean isGoingRight = true;
+        int lines = calculatePossibleLines();
+
+        setKaraToPosition();
 
         // Height of Triangle
-        for (int line = 1; line <= 20; line += 2) {
+        for (int line = 1; line <= lines; line++) {
 
             // place Line
-            for (int i = 0; i < line; i++) {
+            for (int i = 0; i < line * 2 - 1; i++) {
                 kara.putLeaf();
                 kara.move();
             }
